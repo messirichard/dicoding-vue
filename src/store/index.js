373f242d473f71636b4eacafe72486c7d9bf58a4
Richard <richardofay@gmail.com>
2020-05-28 13:31:50 +0700

    done 2

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,12 @@ import Vuex from 'vuex'
 import Vue from 'vue'
 import worldtotal from './modules/worldtotal'
 import listcountry from './modules/listcountry'
-import indtotal from './modules/indtotal'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   modules: {
     worldtotal,
-    listcountry,
-    indtotal
+    listcountry
   }
 })
